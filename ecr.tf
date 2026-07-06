670b85c656b725a89d5154ab7db3d7deeae160de
benjaminglezabg-crypto <benjamingleza.bg@gmail.com>
2026-07-05 22:41:15 -0600

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,35 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project_name}-${var.environment}-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-app"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep the latest 20 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
