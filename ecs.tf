7bb1820613edcde9136f55be61436ad7ba3dbbac
benjaminglezabg-crypto <benjamingleza.bg@gmail.com>
2026-07-05 22:45:21 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,1 +1,120 @@
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/${var.project_name}-${var.environment}"
+  retention_in_days = 14
 
+  tags = {
+    Name = "/ecs/${var.project_name}-${var.environment}"
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-${var.environment}-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-cluster"
+  }
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family = "${var.project_name}-${var.environment}-app"
+
+  requires_compatibilities = [
+    "FARGATE"
+  ]
+
+  network_mode = "awsvpc"
+
+  cpu    = tostring(var.container_cpu)
+  memory = tostring(var.container_memory)
+
+  execution_role_arn = aws_iam_role.ecs_execution.arn
+  task_role_arn      = aws_iam_role.ecs_task.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.container_image
+      essential = true
+
+      portMappings = [
+        {
+          name          = "http"
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+          appProtocol   = "http"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "app"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-app"
+  }
+}
+
+resource "aws_ecs_service" "app" {
+  name = "${var.project_name}-${var.environment}-service"
+
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  platform_version = "LATEST"
+
+  health_check_grace_period_seconds = 60
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  network_configuration {
+    subnets = aws_subnet.private[*].id
+
+    security_groups = [
+      aws_security_group.ecs.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app"
+    container_port   = var.container_port
+  }
+
+  depends_on = [
+    aws_lb_listener.http,
+    aws_iam_role_policy_attachment.ecs_execution
+  ]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-service"
+  }
+}
